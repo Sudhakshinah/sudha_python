1678b777ae9f66030eb309d570e75aad425f6b33
Sudhakshinah <34530188+Sudhakshinah@users.noreply.github.com>
2023-12-31 22:35:48 +0530

    Add files via upload

---
+++ textfile_currenttimestamp.py
@@ -0,0 +1,34 @@
+# Import date and time
+
+from datetime import datetime
+
+# Finding the current date and time
+current_timestamp = datetime.now()
+formatted = current_timestamp.strftime("%Y-%m-%d %H-%M-%S")
+
+
+# function to create a file with name as timestamp and write content as timestamp
+def file_as_timestamp():
+    f1 = open(current_timestamp.strftime("%Y-%m-%d %H-%M-%S")+".txt", "w")
+    f1.write(formatted)
+
+
+file_as_timestamp()
+
+
+# function to read the file
+def readfile1():
+    content = open("2023-12-31 22-28-02.txt", "r")
+    print(content.read())
+
+
+readfile1()
+
+
+
+
+
+
+
+
+
